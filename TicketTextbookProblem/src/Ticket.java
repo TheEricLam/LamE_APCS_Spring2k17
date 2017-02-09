52ec74f6e3a6b21057fe28bf94d9667607a03f07
TheEricLam <18lame45@millshighsch.org>
2017-02-09 07:41:39 -0800

    2.9.17
    Ticket

---
+++ TicketTextbookProblem/src/Ticket.java
@@ -0,0 +1,13 @@
+
+public abstract class Ticket {
+	int ticketnumber;
+	
+	public Ticket (int ticketNumber) {
+		this.ticketnumber = ticketNumber;
+	}
+	
+	// abstract methods
+	public abstract double getPrice();
+	
+	public abstract String toString();
+}
