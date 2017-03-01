cf122e2f89e1cad6cf0ab3c8da67ee70d63d5a3c
TheEricLam <18lame45@millshighsch.org>
2017-03-01 10:42:23 -0800

    TexEdit 3.1.17

---
+++ TextExcel/src/textExcel/EmptyCell.java
@@ -0,0 +1,17 @@
+package textExcel;
+
+public class EmptyCell implements Cell {
+
+	@Override
+	public String abbreviatedCellText() {
+		
+		return null;
+	}
+
+	@Override
+	public String fullCellText() {
+		
+		return null;
+	}
+
+}
