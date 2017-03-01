9ba3f5614238079e0b4a083cc3c15bdfeb7fea0e
TheEricLam <18lame45@millshighsch.org>
2017-03-01 10:44:51 -0800

    3.1.17_2

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class Test {
 
 	public static void main(String[] args) {
-		Scanner 
+		Scanner name = new Scanner("Whats your name");
+		System.out.println(name);
 
 	}
 
