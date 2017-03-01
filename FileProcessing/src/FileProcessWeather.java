ceb039b72a84584ae3c434ce7ff415ff54a88ae0
TheEricLam <18lame45@millshighsch.org>
2017-02-28 23:12:58 -0800

    2.28.17

---
+++ FileProcessing/src/FileProcessWeather.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class FileProcessWeather {
+	public static void main (String[] args) throws FileNotFoundException {
+		Scanner input = new Scanner(new File("./resources/weather"));
+		double currentTemp = input.nextDouble(); //the next input
+		
+		while(input.hasNextDouble()) { //checks if next input is a double
+			double nextTemp = input.nextDouble();
+			double change = nextTemp - currentTemp;
+			System.out.println(currentTemp + " to "  + nextTemp + ", change = " + change);
+			currentTemp = nextTemp; 
+		}
+	}
+}
